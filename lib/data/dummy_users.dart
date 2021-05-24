57464122049c97554f9f2b6cd27a0a1daa206657
DilciHettwer <dilcylopes@gmail.com>
2021-05-24 13:00:02 -0300

    Initial commit

---
+++ lib/data/dummy_users.dart
@@ -0,0 +1,22 @@
+import 'package:flutter_app1/models/user.dart';
+
+const DUMMY_USERS = {
+  '1': const User(
+    id:'1',
+    name: 'Maria',
+    email: 'ma@alunos.com',
+    avatarUrl:'https://cdn.pixabay.com/photo/2016/04/01/12/11/avatar-1300582_960_720.png'
+  ),
+  '2': const User(
+    id:'2',
+    name: 'Pedro',
+    email: 'pedro@alunos.com.br',
+    avatarUrl: 'https://cdn.pixabay.com/photo/2016/03/31/19/58/avatar-1295429_960_720.png'
+  ),
+  '3': const User(
+    id:'3',
+    name: 'Guilherme',
+    email: 'guigui@alunos.com.br',
+    avatarUrl: 'https://cdn.pixabay.com/photo/2016/12/13/16/17/dancer-1904467_960_720.png'
+  )
+};
---
+++ lib/routes/app_routes.dart
@@ -0,0 +1,6 @@
+//para evitar digitar e erar alguma rota
+
+class AppRoutes{
+  static const HOME = '/';
+  static const USER_FORM = '/user-form';
+}
